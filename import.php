2c9f550b620c31f9c33cb10a59b31f83c96960fb
Patsura Dmitry <zaets28rus@gmail.com>
2014-11-16 10:49:25 +0900

    Code style

--- import.php
+++ import.php
@@ -4,7 +4,8 @@ include_once __DIR__ . '/vendor/autoload.php';
 
 set_time_limit(0);
 
-class Profiler extends Stackable {
+class Profiler extends Stackable
+{
     public $total = 0;
 }
 
@@ -24,7 +25,8 @@ class WebWorker extends Worker
      * @param Profiler $profiler
      * @param SafeLog $logger
      */
-    public function __construct(Profiler $profiler, SafeLog $logger) {
+    public function __construct(Profiler $profiler, SafeLog $logger)
+    {
         $this->profiler = $profiler;
         $this->logger = $logger;
     }
@@ -34,7 +36,7 @@ class WebWorker extends Worker
     public function writeToDb(array $values)
     {
         $connection = new PDO('mysql:host=localhost;dbname=vk_import', 'root', 'root');
-        $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $connection->exec("SET CHARACTER SET utf8");
 
         foreach ($values as $row) {
@@ -54,26 +56,28 @@ class WebWorker extends Worker
  *
  * @property WebWorker $worker
  */
-class VkFetchThread extends Thread {
+class VkFetchThread extends Thread
+{
     protected $start;
 
     protected $end;
 
     public function __construct($start, $end)
     {
-        $this->start  = $start;
+        $this->start = $start;
         $this->end = $end;
     }
 
-    public function run() {
+    public function run()
+    {
         $limit = 300;
-        $interval = round($this->end/$limit);
+        $interval = round($this->end / $limit);
 
         for ($i = 0; $i < $interval; $i++) {
-            $start = $i*$limit+1;
-            $end = ($i*$limit)+$limit;
+            $start = $i * $limit + 1;
+            $end = ($i * $limit) + $limit;
 
-            $result = file_get_contents('https://api.vk.com/method/users.get?user_ids='.implode(',', array_keys(array_fill($start, $limit, 1))));
+            $result = file_get_contents('https://api.vk.com/method/users.get?user_ids=' . implode(',', array_keys(array_fill($start, $limit, 1))));
             $this->worker->profiler->total += $limit;
 
             $users = json_decode($result);
@@ -91,7 +95,8 @@ class VkFetchThread extends Thread {
  */
 class ProfilerThread extends Thread
 {
-    public function run() {
+    public function run()
+    {
         while (true) {
             usleep(100000);
             $this->worker->logger->log("Total count {$this->worker->profiler->total}");
@@ -100,8 +105,10 @@ class ProfilerThread extends Thread
 }
 
 
-class SafeLog extends Stackable {
-    public function log($message, $args = []) {
+class SafeLog extends Stackable
+{
+    public function log($message, $args = [])
+    {
         $args = func_get_args();
 
         if (($message = array_shift($args))) {
@@ -113,10 +120,10 @@ class SafeLog extends Stackable {
 
 const FetchVkWorkers = 16;
 
-$pool = new Pool(FetchVkWorkers+1, 'WebWorker', [new Profiler(), new SafeLog()]);
+$pool = new Pool(FetchVkWorkers + 1, 'WebWorker', [new Profiler(), new SafeLog()]);
 
 for ($i = 0; $i < FetchVkWorkers; $i++) {
-    $pool->submit(new VkFetchThread(($i*100000000)+1, ($i*100000000)+100000000));
+    $pool->submit(new VkFetchThread(($i * 100000000) + 1, ($i * 100000000) + 100000000));
 }
 
 $pool->submit(new ProfilerThread());
